efe68900515fa12db7527b2b0bfed72495e8678c
y-as-h <yashc331@gmail.com>
2023-07-25 00:33:19 +0530

    added top down dp

--- DP/longest_common_subsequence.cpp
+++ DP/longest_common_subsequence.cpp
@@ -14,7 +14,25 @@ class Solution
     //Function to find the length of longest common subsequence in two strings.
     int lcs(int n, int m, string s1, string s2)
     {
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return recursion(n, m, s1, s2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));        
+	 // return recursion(n, m, s1, s2, dp);
+
+        for(int i = 0; i <= n; ++i){
+            dp[i][0] = 0;
+        }
+        for(int j = 0; j <= m; ++j){
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1; i <= n; ++i){
+            for(int j = 1; j <= m; ++j){
+                if(s1[i-1] == s2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[n][m];
     }
 };
